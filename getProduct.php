6dad0b11b1c76e621532a2ca9799c916fdc44e9a
DDHL97 <1715110005@utec-tgo.edu.mx>
2017-03-11 21:31:24 -0600

    Update getProduct.php

--- getProduct.php
+++ getProduct.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "localhost";
-$db_name = "mercancia";
-$db_user = "root";
-$db_password = "";
+$db_host = "jnc6n3xpbgc3kek2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+     $db_name = "t97bvexsdrc2901u";
+     $db_user = "cx0cbb132z69nw9j";
+     $db_password = "axlcj5fiygl8zsh0";
 $connection = mysql_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysql_error());
 mysql_select_db($db_name) or die("Error al seleccionar la base de datos:".mysql_error());
 @mysql_query("SET NAMES 'utf8'");
